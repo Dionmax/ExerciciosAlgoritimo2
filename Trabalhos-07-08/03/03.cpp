1b74c0210480f64746599e78791d950db51593fd
Dionmax Nobre <dionmax@edu.univali.br>
2017-08-10 03:58:03 -0300

    Trabalho 07 08

--- Trabalhos-07-08/03/03.cpp
+++ Trabalhos-07-08/03/03.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+#include "string"
 
+using namespace std;
+
+string verificador(int numero)
+{
+	if (numero % 2 == 0)
+		return "par.";
+	else
+		return "ímpar.";
+}
+
+void solicitacao()
+{
+	int numero;
+
+	cout << "Insira um numero: ";
+
+	cin >> numero;
+
+	cout << "O numero inserido é " << verificador(numero);
+
+	cin.get();
+	cin.get();
+}
 
 int main()
 {
+	setlocale(LC_ALL, "Portuguese");
+
+	solicitacao();
+
     return 0;
 }
 
