6748baa0e91acfcc6d453696dc93bb109b2b4fe0
Dionmax Nobre <dionmax@edu.univali.br>
2017-08-10 05:18:17 -0300

    Trabalhos0708

--- Trabalhos-07-08/05/05.cpp
+++ Trabalhos-07-08/05/05.cpp
@@ -7,10 +7,29 @@
 
 using namespace std;
 
+const int TABUADA_LIMITE = 10;
+
+void tabuada()
+{
+	for (int cont = 1; cont <= TABUADA_LIMITE; cont++)
+	{
+		for (int conttwo = 1; conttwo <= TABUADA_LIMITE; conttwo++)
+		{
+			cout << cont << "X" << conttwo << "= " << cont*conttwo << endl;
+		}
+		cout << endl;
+	}
+
+	cin.get();
+	cin.get();
+}
+
 
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
+
+	tabuada();
 
     return 0;
 }
---
+++ Trabalhos-07-08/07/07.cpp
@@ -0,0 +1,25 @@
+// 07.cpp : Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+void pedido()
+{
+	cin.get();
+	cin.get();
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+
+	pedido();
+
+	return 0;
+}
+
