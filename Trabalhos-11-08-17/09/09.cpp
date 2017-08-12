3b20037bc5b902ab18ac077b0ab448a85a304b96
Dionmax Nobre <dionmax@edu.univali.br>
2017-08-11 21:55:34 -0300

    Trabalhos-11-08-17

--- Trabalhos-11-08-17/09/09.cpp
+++ Trabalhos-11-08-17/09/09.cpp
@@ -13,7 +13,38 @@ const int TAMANHO = 10;
 
 void pedido()
 {
-	cout <<  
+	int n_um[TAMANHO],
+		n_dois[TAMANHO],
+		n_resul[TAMANHO];
+
+	cout << "Numeros gerados aleatóriamente: " << endl;
+
+	for (int cont = 0; cont < TAMANHO; cont++)
+		n_um[cont] = rand() % 10;
+
+	cout << "Os numeros aleatorio gerado para o array um foram: " << endl;
+
+	for (int cont = 0; cont < TAMANHO; cont++)
+		cout << n_um[cont] << " ";
+
+	for (int cont = 0; cont < TAMANHO; cont++)
+		n_dois[cont] = rand() % 10;
+
+	cout << endl << "Os numeros aleatorio gerado para o array dois foram: " << endl;
+
+	for (int cont = 0; cont < TAMANHO; cont++)
+		cout << n_dois[cont] << " ";
+
+	for (int cont = 0; cont < TAMANHO; cont++)
+		n_resul[cont] = n_um[cont] * n_dois[cont];
+
+	cout << endl << "O resultado da multiplicação dos números foram: " << endl;
+	
+	for (int cont = 0; cont < TAMANHO; cont++)
+		cout << n_resul[cont] << endl;
+
+	cin.get();
+	cin.get();
 }
 
 int main()
