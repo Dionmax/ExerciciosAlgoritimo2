a3bb3ebc49ac2554ae06637e746e28c05b0ead3d
Dionmax Nobre <dionmax@edu.univali.br>
2017-08-11 21:34:23 -0300

    Trabalhos-11-8

--- Trabalhos-11-08-17/01/01.cpp
+++ Trabalhos-11-08-17/01/01.cpp
@@ -2,10 +2,30 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+#include "string"
 
+using namespace std;
+
+const int TAMANHO = 50;
+
+void contador()
+{
+	for (int cont = 0; cont <= TAMANHO; cont++)
+	{
+		cout << cont << endl;
+	}
+
+	cin.get();
+	cin.get();
+}
 
 int main()
 {
+	setlocale(LC_ALL, "Portuguese");
+
+	contador();
+
     return 0;
 }
 
---
+++ Trabalhos-11-08-17/09/09.cpp
@@ -0,0 +1,29 @@
+// 09.cpp : Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "string"
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+const int TAMANHO = 10;
+
+void pedido()
+{
+	cout <<  
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+
+	srand(time (NULL));
+
+	pedido();
+
+    return 0;
+}
+
