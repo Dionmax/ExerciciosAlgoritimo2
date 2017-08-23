9e86787c21681090e19b4a2e79ca370963ed5462
Dionmax Nobre <dionmax@edu.univali.br>
2017-08-23 00:36:19 -0300

    Trabalho-ACC1-21-08

---
+++ Trabalho-ACC1-21-08/Função-teste/AtividadeVetores.h
@@ -0,0 +1,187 @@
+#ifndef ATIVIDADESVETORES_H
+#define ATIVIDADESVETORES_H
+
+#define TAMANHO_MATRIZ 5
+
+#define ESCOPO_BUSCA_MATRIZ_DIAGONALPRINCIPAL 1
+#define ESCOPO_BUSCA_MATRIZ_DIAGONALSECUNDARIA 2
+#define ESCOPO_BUSCA_MATRIZ_DIAGONAIS 3
+#define ESCOPO_BUSCA_MATRIZ_COMPLETA 4
+
+void bubbleSort(int vetor[], int tamanho, bool ordenacaoCrescente);
+
+int maiorValorEmVetor(int vetor[], int tamanho);
+
+int menorValorEmVetor(int vetor[], int tamanho);
+
+int maiorValorEmMatriz(int matriz[TAMANHO_MATRIZ][TAMANHO_MATRIZ], int escopoBusca);
+
+int menorValorEmMatriz(int matriz[TAMANHO_MATRIZ][TAMANHO_MATRIZ], int escopoBusca);
+
+#include "stdafx.h"
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+void bubbleSort(int vetor[], int tamanho, bool ordenacaoCrescente)
+{
+	int aux;
+
+	for (int contador = 0; contador < tamanho; contador++)
+		for (int indice = 0; indice < tamanho - 1; indice++)
+		{
+			aux = vetor[indice];
+
+			if (vetor[indice] < vetor[indice + 1])
+			{
+				vetor[indice] = vetor[indice + 1];
+				vetor[indice + 1] = aux;
+			}
+		}
+
+	if (ordenacaoCrescente)
+		for (int contador = tamanho-1; contador >= 0; contador--)
+			cout << vetor[contador] << " ";
+	else
+		for (int contador = 0; contador < tamanho; contador++)
+			cout << vetor[contador] << " ";
+}
+
+int maiorValorEmVetor(int vetor[], int tamanho)
+{
+	int maiorValor = 0;
+
+	for (int contador = 0; contador < tamanho; contador++)
+	{
+		if (vetor[contador] > maiorValor || contador == 0)
+			maiorValor = vetor[contador];
+	}
+
+	return maiorValor;
+}
+
+int menorValorEmVetor(int vetor[], int tamanho)
+{
+	int menorValor = 0;
+
+	for (int contador = 0; contador < tamanho; contador++)
+	{
+		if (vetor[contador] < menorValor || contador == 0)
+			menorValor = vetor[contador];
+	}
+
+	return menorValor;
+}
+
+int maiorValorEmMatriz(int matriz[TAMANHO_MATRIZ][TAMANHO_MATRIZ], int escopoBusca)
+{
+	int maiorValor = 0;
+
+	if (escopoBusca == ESCOPO_BUSCA_MATRIZ_DIAGONALPRINCIPAL)
+	{
+		for (int contador = 0; contador < TAMANHO_MATRIZ; contador++)
+			for (int indice = 0; indice < TAMANHO_MATRIZ; indice++)
+			{
+				if (contador == indice)
+					if (matriz[contador][indice] > maiorValor || contador == 0)
+						maiorValor = matriz[contador][indice];
+			}
+	}
+
+	else if (escopoBusca == ESCOPO_BUSCA_MATRIZ_DIAGONALSECUNDARIA)
+	{
+		for (int contador = 0; contador < TAMANHO_MATRIZ; contador++)
+			for (int indice = 0; indice < TAMANHO_MATRIZ; indice++)
+			{
+				if ((TAMANHO_MATRIZ-1) - contador == indice)
+					if (matriz[contador][indice] > maiorValor || contador == 0)
+						maiorValor = matriz[contador][indice];
+			}
+	}
+
+	else if (escopoBusca == ESCOPO_BUSCA_MATRIZ_DIAGONAIS)
+	{
+		for (int contador = 0; contador < TAMANHO_MATRIZ; contador++)
+			for (int indice = 0; indice < TAMANHO_MATRIZ; indice++)
+			{
+				if (contador == indice)
+					if (matriz[contador][indice] > maiorValor || contador == 0)
+						maiorValor = matriz[contador][indice];
+
+				if ((TAMANHO_MATRIZ - 1) - contador == indice)
+					if (matriz[contador][indice] > maiorValor)
+						maiorValor = matriz[contador][indice];
+			}
+	}
+
+	else if (escopoBusca == ESCOPO_BUSCA_MATRIZ_COMPLETA)
+	{
+		for (int contador = 0; contador < TAMANHO_MATRIZ; contador++)
+			for (int indice = 0; indice < TAMANHO_MATRIZ; indice++)
+			{
+				if (matriz[contador][indice] > maiorValor || contador == 0)
+					maiorValor = matriz[contador][indice];
+			}
+	}
+
+	return maiorValor;
+}
+
+int menorValorEmMatriz(int matriz[TAMANHO_MATRIZ][TAMANHO_MATRIZ], int escopoBusca)
+{
+	int menorValor = 0;
+
+	if (escopoBusca == ESCOPO_BUSCA_MATRIZ_DIAGONALPRINCIPAL)
+	{
+		for (int contador = 0; contador < TAMANHO_MATRIZ; contador++)
+			for (int indice = 0; indice < TAMANHO_MATRIZ; indice++)
+			{
+				if (contador == indice)
+					if (matriz[contador][indice] < menorValor || contador == 0)
+						menorValor = matriz[contador][indice];
+			}
+	}
+
+	else if (escopoBusca == ESCOPO_BUSCA_MATRIZ_DIAGONALSECUNDARIA)
+	{
+		for (int contador = 0; contador < TAMANHO_MATRIZ; contador++)
+			for (int indice = 0; indice < TAMANHO_MATRIZ; indice++)
+			{
+				if ((TAMANHO_MATRIZ - 1) - contador == indice)
+					if (matriz[contador][indice] < menorValor || contador == 0)
+						menorValor = matriz[contador][indice];
+			}
+	}
+
+	else if (escopoBusca == ESCOPO_BUSCA_MATRIZ_DIAGONAIS)
+	{
+		for (int contador = 0; contador < TAMANHO_MATRIZ; contador++)
+			for (int indice = 0; indice < TAMANHO_MATRIZ; indice++)
+			{
+				if (contador == indice)
+					if (matriz[contador][indice] < menorValor || contador == 0)
+						menorValor = matriz[contador][indice];
+
+				if ((TAMANHO_MATRIZ - 1) - contador == indice)
+					if (matriz[contador][indice] < menorValor)
+						menorValor = matriz[contador][indice];
+			}
+	}
+
+	else if (escopoBusca == ESCOPO_BUSCA_MATRIZ_COMPLETA)
+	{
+		for (int contador = 0; contador < TAMANHO_MATRIZ; contador++)
+			for (int indice = 0; indice < TAMANHO_MATRIZ; indice++)
+			{
+				if (matriz[contador][indice] < menorValor || contador == 0)
+					menorValor = matriz[contador][indice];
+			}
+	}
+
+	return menorValor;
+}
+
+#endif // !ATIVIDADESVETORES_H
+
+#pragma once
--- Trabalho-ACC1-21-08/Função-teste/ReadMe.txt
+++ Trabalho-ACC1-21-08/Função-teste/ReadMe.txt
@@ -1,27 +1,27 @@
 ========================================================================
-    CONSOLE APPLICATION : FuncaoDeTeste Project Overview
+    CONSOLE APPLICATION : Função-teste Project Overview
 ========================================================================
 
-AppWizard has created this FuncaoDeTeste application for you.
+AppWizard has created this Função-teste application for you.
 
 This file contains a summary of what you will find in each of the files that
-make up your FuncaoDeTeste application.
+make up your Função-teste application.
 
 
-FuncaoDeTeste.vcxproj
+Função-teste.vcxproj
     This is the main project file for VC++ projects generated using an Application Wizard.
     It contains information about the version of Visual C++ that generated the file, and
     information about the platforms, configurations, and project features selected with the
     Application Wizard.
 
-FuncaoDeTeste.vcxproj.filters
+Função-teste.vcxproj.filters
     This is the filters file for VC++ projects generated using an Application Wizard. 
     It contains information about the association between the files in your project 
     and the filters. This association is used in the IDE to show grouping of files with
     similar extensions under a specific node (for e.g. ".cpp" files are associated with the
     "Source Files" filter).
 
-FuncaoDeTeste.cpp
+Funçãoteste.cpp
     This is the main application source file.
 
 /////////////////////////////////////////////////////////////////////////////
@@ -29,7 +29,7 @@ Other standard files:
 
 StdAfx.h, StdAfx.cpp
     These files are used to build a precompiled header (PCH) file
-    named FuncaoDeTeste.pch and a precompiled types file named StdAfx.obj.
+    named Funçãoteste.pch and a precompiled types file named StdAfx.obj.
 
 /////////////////////////////////////////////////////////////////////////////
 Other notes:
