c392cf8762189670e9adcf36bac69af1cabe5fa8
Dionmax Nobre <dionmax@edu.univali.br>
2017-09-01 03:34:24 -0300

    trabalho-04-09

---
+++ TrabalhoJogoDaVelha_04-09/TrabalhoJogoDaVelha(JV)/FuncoesJogoDaVelha.h
@@ -0,0 +1,7 @@
+#ifndef FUNCOESJOGODAVELHA_H
+#define FUNCOESJOGODAVELHA_H
+
+#include "stdafx.h"
+
+#endif // !FUNCOESJOGODAVELHA_H
+
--- TrabalhoJogoDaVelha_04-09/TrabalhoJogoDaVelha(JV)/TrabalhoJogoDaVelhaJV.cpp
+++ TrabalhoJogoDaVelha_04-09/TrabalhoJogoDaVelha(JV)/TrabalhoJogoDaVelhaJV.cpp
@@ -1,5 +1,5 @@
-// TrabalhoJogoDaVelhaJV.cpp: Define o ponto de entrada para a aplicação de console. -> 
-// #4 JV = Joe Vega -> Ativo em modo de ataque o Capitão referência! h ttp://nerdpai.com/wp-content/uploads/2016/06/capitao-am%C3%A9rica-referencia.jpgnerdpai.com/wp-content/uploads/2016/06/capitao-am%C3%A9rica-referencia.jpg
+// TrabalhoJogoDaVelhaJV.cpp: Define o ponto de entrada para a aplicação de console.
+// #4 JV = Joe Vega -> Ativo em modo de ataque o Capitão referência!
 
 #include "stdafx.h"
 
