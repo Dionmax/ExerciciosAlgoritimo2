f9972216cade48c326a9593b61709f14943ad122
Dionmax Nobre <dionmax@edu.univali.br>
2017-09-01 05:52:38 -0300

    Trabalho-04-09-InProgress

--- TrabalhoJogoDaVelha_04-09/TrabalhoJogoDaVelha(JV)/FuncoesJogoDaVelha.h
+++ TrabalhoJogoDaVelha_04-09/TrabalhoJogoDaVelha(JV)/FuncoesJogoDaVelha.h
@@ -2,6 +2,55 @@
 #define FUNCOESJOGODAVELHA_H
 
 #include "stdafx.h"
+#include "string"
+
+#define TAMANHO_MATRIZ_JOGO 3
+#define JOGADOR_X 'X'
+#define JOGADOR_O 'O'
+
+using namespace std;
+
+bool alocar_jogada(string matriz_jogo[][TAMANHO_MATRIZ_JOGO], char jogador,int casa_jogada)
+{
+
+}
+
+void escrever_ordem_casas()
+{
+	int matriz_location[TAMANHO_MATRIZ_JOGO][TAMANHO_MATRIZ_JOGO],
+		ponteiro_matriz = 0;
+
+	for (int coluna = 0; coluna < TAMANHO_MATRIZ_JOGO; coluna++)
+		for (int linha = 0; linha < TAMANHO_MATRIZ_JOGO; linha++)
+		{
+			ponteiro_matriz = coluna * TAMANHO_MATRIZ_JOGO + linha;
+			matriz_location[coluna][linha] = ponteiro_matriz + 1;
+		}
+
+	for (int cont = 0; cont < TAMANHO_MATRIZ_JOGO; cont++)
+	{
+		cout << "|";
+
+		for (int cont2 = 0; cont2 < TAMANHO_MATRIZ_JOGO; cont2++)
+			cout << matriz_location[cont][cont2] << "	|";
+
+		cout << endl;
+	}
+}
+
+void escrever_matriz(string matriz_jogo[][TAMANHO_MATRIZ_JOGO])
+{
+	for (int cont = 0; cont < TAMANHO_MATRIZ_JOGO; cont++)
+	{
+		cout << "|";
+
+		for (int cont2 = 0; cont2 < TAMANHO_MATRIZ_JOGO; cont2++)
+			cout << matriz_jogo[cont][cont2] << "	|";
+
+		cout << endl;
+	}
+}
+
 
 #endif // !FUNCOESJOGODAVELHA_H
 
--- TrabalhoJogoDaVelha_04-09/TrabalhoJogoDaVelha(JV)/TrabalhoJogoDaVelhaJV.cpp
+++ TrabalhoJogoDaVelha_04-09/TrabalhoJogoDaVelha(JV)/TrabalhoJogoDaVelhaJV.cpp
@@ -1,11 +1,34 @@
 // TrabalhoJogoDaVelhaJV.cpp: Define o ponto de entrada para a aplicação de console.
-// #4 JV = Joe Vega -> Ativo em modo de ataque o Capitão referência!
+// #4 JV = Joe Vega -> Invoco o Capitão referência em modo de ataque! 
 
 #include "stdafx.h"
+#include "iostream"
+#include "string"
+#include <locale.h>
+#include <stdio.h>
 
+#include "FuncoesJogoDaVelha.h"
+
+using namespace std;
+
+bool andamento_jogo(string matriz_jogo[][TAMANHO_MATRIZ_JOGO])
+{
+	
+}
+
+void inicio_jogo()
+{
+	string matriz_jogo[TAMANHO_MATRIZ_JOGO][TAMANHO_MATRIZ_JOGO];
+
+	andamento_jogo(matriz_jogo);
+}
 
 int main()
 {
+	inicio_jogo();
+
+	cin.get();
+
     return 0;
 }
 
