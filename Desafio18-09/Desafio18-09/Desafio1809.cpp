704374a4f155d48084c8d2d5484187212b2c2b93
Dionmax Nobre <dionmax@edu.univali.br>
2017-09-21 15:34:19 -0300

    MyMindIsBroken

--- Desafio18-09/Desafio18-09/Desafio1809.cpp
+++ Desafio18-09/Desafio18-09/Desafio1809.cpp
@@ -9,19 +9,21 @@
 
 using namespace std;
 
+void buscando_igualdade(int matriz[TAMANHO_MATRIZ][TAMANHO_MATRIZ])
+{
+	for (int linha = 0; linha < TAMANHO_MATRIZ; linha++)
+	{
+		for (int coluna = 0; coluna < TAMANHO_MATRIZ; coluna++)
+		{
+
+		}
+	}
+		
+}
+
 int main()
 {
-	int matriz[TAMANHO_MATRIZ][TAMANHO_MATRIZ];
-
-	bool teste = false;
-
-	for (int linha = 0; linha < TAMANHO_MATRIZ; linha++)
-		for (int coluna = 0; coluna < TAMANHO_MATRIZ; coluna++)
-			matriz[linha][coluna] = 1;
-
-	teste = verificar_igualdade(matriz);
-
-	cout << teste;
+	int matriz[TAMANHO_MATRIZ][TAMANHO_MATRIZ] = {1,2,3,4,5,6,7,8,9};
 
 	cin.get();
 
