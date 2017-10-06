b559f9053adc82da46e9f60bc6ac487da55dd83e
Dionmax Nobre <dionmax@edu.univali.br>
2017-10-06 20:09:57 -0300

    ACC2

---
+++ TrabalhoACC2-Simulador/TrabalhoACC2-Simulador/TrabalhoACC2Simulador.cpp
@@ -0,0 +1,24 @@
+// TrabalhoACC2Simulador.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+#include "Flood.h"
+
+using namespace std;
+
+int main()
+{
+	escreverUmDosCampos(TAMANHO_PEQUENO);
+	cout << endl;
+
+	escreverUmDosCampos(TAMANHO_MEDIO);
+	cout << endl;
+
+	escreverUmDosCampos(TAMANHO_GRANDE);
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
