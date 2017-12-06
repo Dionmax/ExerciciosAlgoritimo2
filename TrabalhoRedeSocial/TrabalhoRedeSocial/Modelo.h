5fc1d74a899c9f62188df6774be36e62f85cd597
Dionmax Nobre <dionmax@edu.univali.br>
2017-12-06 02:01:34 -0200

    Start.

---
+++ TrabalhoRedeSocial/TrabalhoRedeSocial/Modelo.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+struct Usuarios
+{
+	string nome;
+};
---
+++ TrabalhoRedeSocial/TrabalhoRedeSocial/SuporteEscrita.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+void escrever(T algo)
+{
+	cout << algo << endl;
+}
